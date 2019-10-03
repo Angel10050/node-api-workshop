15770e877ab5a0cc5d443c129efbe9034bde97bf
angel10050 <angelangel10050@gmail.com>
2019-10-03 14:57:31 -0500

    complete to the end, separate by folders and files to exported

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
 
-const albumsData = [
+let albumsData = [
     {
       albumId: "10",
       artistName: "Beyoncé",
@@ -20,7 +21,7 @@ const albumsData = [
       artworkUrl100:
         "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
       releaseDate: "2003-06-24T07:00:00Z",
-      primaryGenreName: "Pop",
+      primaryGenreName: "Rock",
       url:
         "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0"
     },
@@ -31,26 +32,55 @@ const albumsData = [
     artworkUrl100:
       "https://upload.wikimedia.org/wikipedia/en/b/b1/Beyonce_-_Above_and_Beyonce.png",
     releaseDate: "2009-06-16",
-    primaryGenreName: "Pop",
+    primaryGenreName: "Jazz",
     url:
       "https://www.youtube.com/watch?v=ip9KSqj0ErI"
   }
   ];
 
-app.get("/albums", ( req, res ) => {
-    res.send(albumsData)
-})
+app.use(bodyParser.json())
 
-app.get("/albums/:albumId", function( req, res ) {
- const {albumId} = req.params
- const search = albumsData.find(albu => albu.albumId == albumId)
+app.get("/albums", function ( req, res ) {
+  const {primaryGenreName} = req.query // FILTER by query strings parameters
+  console.log(primaryGenreName)
+  const search = albumsData.find(albu => albu.primaryGenreName === primaryGenreName)
+   return res.status(200).json(search)
+ })
+
+app.get("/albums/:albumId", function ( req, res ) {
+ const {albumId} = req.params // filter by parameters
+ const search = albumsData.find(albu => albu.albumId == parseInt(albumId))
   return res.status(200).json(search)
 })
 
-app.put("/albums", function(req, res) {
+app.post("/albums", function( req, res) {
     console.log("POST /albums route");
+    console.log(req.body) // *req.body* Esto contiene el valor de la solicitud, osea el valor de lo que se va a enviar en este caso un Obj
+    albumsData.push(req.body)
+    res.json(albumsData)
   });
+
+app.delete("/albums/:albumId", function ( req, res ){
+    const { albumId } = req.params
+
+    const copy = [...albumsData]
+    albumsData = albumsData.filter(element => element.albumId !== albumId)
+    if (copy.length > albumId.length) {
+      return res.status(200).json(albumsData)
+    }
+    return res.status(404).json('Not found')
+})
+
+app.put("/albums/:albumId", function ( req, res ) {
+  const {albumId} = req.params  
+   const change = albumsData.map(album => album.albumId == albumId ? album = req.body : album)
+   albumsData = change
+    res.status(200).json(change)
+})
+
+
 
 app.listen(8585, () => {
     console.log("This Server Is Running On Port # 8585");  
 })
+
